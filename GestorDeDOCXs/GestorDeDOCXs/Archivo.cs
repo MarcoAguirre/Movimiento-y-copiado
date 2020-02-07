c0d3ee10539afb4d4e262d9702dafc6d533e8ac9
MarcoAguirre <viniciosoria16@gmail.com>
2020-02-07 00:02:47 -0500

    Mueve o copia archivos.
    Se corrigieron varios errores conocidos.

---
+++ GestorDeDOCXs/GestorDeDOCXs/Archivo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestorDeDOCXs
+{
+    public class Archivo
+    {
+        private string m_strNombreDelArchivo = "";
+        private string m_strRutaDeOrigen = "";
+        private DateTime m_dtFechaDeUltimaModificacion = new DateTime();
+
+        public string NombreDelArchivo
+        {
+            get => m_strNombreDelArchivo;
+            set => m_strNombreDelArchivo = value;
+        }
+
+        public string RutaDeOrigen
+        {
+            get => m_strRutaDeOrigen;
+            set => m_strRutaDeOrigen = value;
+        }
+
+        public DateTime FechaDeUltimaModificacion
+        {
+            get => m_dtFechaDeUltimaModificacion;
+            set => m_dtFechaDeUltimaModificacion = value;
+        }
+    }
+}
---
+++ GestorDeDOCXs/GestorDeDOCXs/MainWindow.xaml.cs
@@ -0,0 +1,201 @@
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GestorDeDOCXs
+{
+    public partial class MainWindow : Window
+    {
+        CommonOpenFileDialog openFileDialog = new CommonOpenFileDialog()
+        {
+            Multiselect = true,
+            EnsurePathExists = true,
+            EnsureFileExists = false,
+            AllowNonFileSystemItems = false,
+            InitialDirectory = "c:\\"
+        };
+
+        HashSet<Archivo> m_hsArchivosSeleccionados = new HashSet<Archivo>();
+        HashSet<string> m_hsRutasDeCadaArchivo = new HashSet<string>();
+
+        string m_strRutaDeDestino = "";
+
+        List<FileInfo> m_lstElementosDeLaRutaDeDestino = new List<FileInfo>();
+
+        public HashSet<Archivo> ArchivosSeleccionados 
+        { 
+            get => m_hsArchivosSeleccionados; 
+            set => m_hsArchivosSeleccionados = value; 
+        }
+
+        public HashSet<string> RutasDeCadaArchivo 
+        { 
+            get => m_hsRutasDeCadaArchivo; 
+            set => m_hsRutasDeCadaArchivo = value; 
+        }
+
+        public string RutaDeDestino 
+        { 
+            get => m_strRutaDeDestino; 
+            set => m_strRutaDeDestino = value; 
+        }
+
+        public List<FileInfo> ElementosDeLaRutaDeDestino 
+        { 
+            get => m_lstElementosDeLaRutaDeDestino; 
+            set => m_lstElementosDeLaRutaDeDestino = value; 
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public HashSet<Archivo> ObtenerLaInformacionDeLosArchivos(HashSet<string> hsRutasObtenidas)
+        {
+            HashSet<Archivo> hsArchivosParaMostrar = new HashSet<Archivo>();
+
+            if (hsRutasObtenidas == null || hsRutasObtenidas.Count == 0) return null;
+
+            foreach (string strRutaDeArchivo in hsRutasObtenidas)
+            {
+                if (string.IsNullOrEmpty(strRutaDeArchivo) || 
+                    string.IsNullOrWhiteSpace(strRutaDeArchivo)) continue;
+
+                DirectoryInfo directoryInfo = new DirectoryInfo(strRutaDeArchivo);
+
+                Archivo archivo = new Archivo()
+                {
+                    NombreDelArchivo = directoryInfo.Name,
+                    RutaDeOrigen = directoryInfo.FullName,
+                    FechaDeUltimaModificacion = directoryInfo.LastAccessTime
+                };
+
+                if (ArchivosSeleccionados.Contains(archivo))
+                {
+                    MessageBox.Show("El archivo" + archivo.NombreDelArchivo + "ya fue ingresado",
+                        "Gestor de DOCXs", MessageBoxButton.OK, MessageBoxImage.Information);
+                    continue;
+                }
+                else
+                    hsArchivosParaMostrar.Add(archivo);
+            }
+
+            return hsArchivosParaMostrar;
+        }
+
+        private void btnFijarRutaDeDestino_Click(object sender, RoutedEventArgs e)
+        {
+            openFileDialog.IsFolderPicker = true;
+            openFileDialog.Multiselect = false;
+            openFileDialog.Title = "Seleccione el lugar donde quiera dejar sus archivos";
+
+            if (openFileDialog.ShowDialog() == CommonFileDialogResult.Ok &&
+                openFileDialog.FileName != null)
+                RutaDeDestino = openFileDialog.FileName;
+            else
+                return;
+
+            txtRutaDeDestino.Text = RutaDeDestino;
+        }
+
+        private void btnExaminar_Click(object sender, RoutedEventArgs e)
+        {
+            openFileDialog.IsFolderPicker = false;
+            openFileDialog.Multiselect = true;
+            openFileDialog.Title = "Seleccione los archivos con los que quiera trabajar";
+
+            if (openFileDialog.ShowDialog() == CommonFileDialogResult.Ok &&
+                openFileDialog.FileNames != null)
+                RutasDeCadaArchivo = openFileDialog.FileNames.ToHashSet();
+            else
+                return;
+
+            ArchivosSeleccionados = ObtenerLaInformacionDeLosArchivos(RutasDeCadaArchivo);
+            lvFiles.ItemsSource = ArchivosSeleccionados;
+            lvFiles.Items.Refresh();
+        }
+
+        //Aqui van los eventos de copiado o movimiento
+
+        private void btnLimpiar_Click(object sender, RoutedEventArgs e)
+        {
+            ArchivosSeleccionados.Clear();
+            RutasDeCadaArchivo.Clear();
+            lvFiles.ItemsSource = null;
+            txtRutaDeDestino.Text = "Seleccione el lugar donde quiera copiar o mover los archivos";
+            lvFiles.Items.Refresh();
+        }
+
+        private void btnCerrar_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnCopiar_Click(object sender, RoutedEventArgs e)
+        {
+            if (ArchivosSeleccionados.Count == 0 || ArchivosSeleccionados == null)
+            {
+                MessageBox.Show("No hay elementos para trabajar", "Gestor de DOCXs",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            foreach (Archivo aArchivosParaCopiar in ArchivosSeleccionados)
+            {
+                if (aArchivosParaCopiar == null) continue;
+
+                DirectoryInfo directoryInfo = new DirectoryInfo(RutaDeDestino);
+
+                ElementosDeLaRutaDeDestino = directoryInfo.GetFiles(
+                    aArchivosParaCopiar.NombreDelArchivo).ToList();
+
+                if (ElementosDeLaRutaDeDestino.Count != 0)
+                {
+                    MessageBox.Show("El archivo " + aArchivosParaCopiar.NombreDelArchivo +
+                        " ya existe en el directorio asignado", "Gestor de DOCXs",
+                        MessageBoxButton.OK, MessageBoxImage.Warning);
+                    continue;
+                }
+
+                File.Copy(aArchivosParaCopiar.RutaDeOrigen,
+                    RutaDeDestino + "\\" + aArchivosParaCopiar.NombreDelArchivo);
+            }
+
+            MessageBox.Show("Proceso terminado exitosamente");
+        }
+
+        private void btnMover_Click(object sender, RoutedEventArgs e)
+        {
+            if (ArchivosSeleccionados.Count == 0 || ArchivosSeleccionados == null)
+            {
+                MessageBox.Show("No hay elementos para trabajar", "Gestor de DOCXs", 
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            foreach (Archivo aArchivosParaCopiar in ArchivosSeleccionados)
+            {
+                if (aArchivosParaCopiar == null) continue;
+
+                File.Move(aArchivosParaCopiar.RutaDeOrigen,
+                RutaDeDestino + "\\" + aArchivosParaCopiar.NombreDelArchivo);
+            }
+
+            MessageBox.Show("Proceso terminado exitosamente");
+        }
+    }
+}
